b09c6a040b3dce5ac270b5e4b7274fb0e53988be
Jon Zgoda <jon@zgoda.us>
2013-05-16 21:43:31 -0500

    Ch 7 completed.
    I didn't agree with posting server-end code in a client accessible
    file…not sure of the motivation behind that.
    But, hadn't used spin before, that's kinda nifty.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -5,5 +5,19 @@ Meteor.Router.add({
 		and: function(id) {
 			Session.set('currentPostId', id);
 		}
+	},
+	'/submit': 'postSubmit'
+});
+
+Meteor.Router.filters({
+	'requireLogin': function(page) {
+		if(Meteor.user())
+			return page;
+		else if(Meteor.loggingIn())
+			return 'loading';
+		else
+			return 'accessDenied';
 	}
 });
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
